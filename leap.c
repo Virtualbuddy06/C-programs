94f253c3ace49c423c4b0df30f4408cd362d6011
Virtualbuddy06 <coderkrish1@gmail.com>
2024-08-24 01:40:23 +0530

    leap.c

--- leap.c
+++ leap.c
@@ -1,31 +1,23 @@
+// Leap Year Program without else if
+//Logic
+//If the year is divisible by 4 it's a leap year.
+//If the year is also 100 it's should be divisible by 400
 #include <stdio.h>
-
 int main() {
-    int leap,c=0;
+    int leap,d=0;
     printf("Enter the Year\n");
     scanf("%d",&leap);
-    if(leap%400==0)
+    if((leap%4==0)&&(leap%100!=0))
     {
-        c=c+1;
+        printf("%d is a leap year",leap);
     }
-    if(leap%4==0)
-    {
-        if(leap%100==0)
-    {
-        printf("%d is not leap year\n",leap);
-    }
-    else
-    {
-        c=c+1;
-    }
-    }
-    else
-    {
-        printf("%d is not a leap year\n",leap);
-    }
-    if(c>=1)
+    if((leap%4==0)&&(leap%100==0)&&(leap%400==0))
     {
         printf("%d is a leap year\n",leap);
+    }
+    if(leap%4!=0)
+    {
+        printf("%d is not a leap year\n",leap);
     }
     return 0;
 }
