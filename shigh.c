34abc3cbb11f4dbc099c2bdf67f581b12b842645
Virtualbuddy06 <coderkrish1@gmail.com>
2025-01-24 08:49:31 +0530

    Add files via upload

---
+++ shigh.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    int size, i;
+    int max1, max2;
+    printf("Enter size of the array (1-1000): ");
+    scanf("%d", &size);
+    int arr[size];
+    
+    printf("Enter elements in the array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    max1 = max2 = 0;
+
+    for(i=0; i<size; i++)
+    {
+        if(arr[i] > max1)
+        {
+          
+            max2 = max1;
+            max1 = arr[i];
+        }
+        else if(arr[i] > max2 && arr[i] < max1)
+        {
+          
+            max2 = arr[i];
+        }
+    }
+
+    printf("First largest = %d\n", max1);
+    printf("Second largest = %d", max2);
+
+    return 0;
+}
